5ca953c6ef0e5948d1ab04737016117522b9205c
Nastia Polina <ap7285@nyu.edu>
2022-07-14 10:49:00 -0700

    final version of TrainModel, documentation editing

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MPL-REU-2022
-Code aggregation for the UNR MPL REU in 2022.
+
+The python file for swapping features is swap_features.py
--- TrainModel/load_data.py
+++ TrainModel/load_data.py
@@ -4,14 +4,13 @@ import random
 import pandas as pd
 import torch
 from math import floor
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 import torchvision
 from torchvision.transforms import functional as tf
-import matplotlib.pyplot as plt
 
 
-class TrainDataset(Dataset):
-    def __init__(self, transform=None):
+class AugmentedDataset(Dataset):
+    def __init__(self, split, transform=None):
         self.transform = transform
         attr_df = pd.read_csv('list_attr_celeba.csv')
 
@@ -27,83 +26,10 @@ class TrainDataset(Dataset):
         os.chdir(path)
         self.image_names = []
         self.image_names_ref = {}
-        for im in range(1, 162771):
-            augmented_image = []
-            # find all images corresponding to the
-            for file in glob.glob(path + str(im) + '/*.jpg'):
-                augmented_image.append(file)
-            self.image_names_ref[im] = augmented_image
-            self.image_names.append(im)
 
-    def __len__(self):
-        return len(self.image_names)
-
-    def __getitem__(self, idx):
-        index = self.image_names[idx]
-        possible_images = self.image_names_ref[index]
-        img_path = possible_images[random.randint(0, len(possible_images) - 1)]
-        image = torchvision.io.read_image(img_path)
-        image = tf.convert_image_dtype(image, torch.float)
-        gender = self.labels[int(img_path[-10:-4])]
-        return image, gender
-
-
-class ValidateDataset(Dataset):
-    def __init__(self, transform=None):
-        self.transform = transform
-        attr_df = pd.read_csv('list_attr_celeba.csv')
-
-        # get names, gender and create labels
-        img_names = attr_df.image_name.values.tolist()
-        img_names = [int(v[:-4]) for v in img_names]
-        img_gender = attr_df.Male.values.tolist()
-        img_gender = [int(v > 0) for v in img_gender]
-        self.labels = dict(zip(img_names, img_gender))
-
-        # add image names with the entire path to them
-        path = '/home/guest/MPL-REU-2022/swapped_attributes/eyes/'
-        os.chdir(path)
-        self.image_names = []
-        self.image_names_ref = {}
-        for im in range(162771, 182638):
-            augmented_image = []
-            # find all images corresponding to the
-            for file in glob.glob(path + str(im) + '/*.jpg'):
-                augmented_image.append(file)
-            self.image_names_ref[im] = augmented_image
-            self.image_names.append(im)
-
-    def __len__(self):
-        return len(self.image_names)
-
-    def __getitem__(self, idx):
-        index = self.image_names[idx]
-        possible_images = self.image_names_ref[index]
-        img_path = possible_images[random.randint(0, len(possible_images) - 1)]
-        image = torchvision.io.read_image(img_path)
-        image = tf.convert_image_dtype(image, torch.float)
-        gender = self.labels[int(img_path[-10:-4])]
-        return image, gender
-
-
-class TestDataset(Dataset):
-    def __init__(self, transform=None):
-        self.transform = transform
-        attr_df = pd.read_csv('list_attr_celeba.csv')
-
-        # get names, gender and create labels
-        img_names = attr_df.image_name.values.tolist()
-        img_names = [int(v[:-4]) for v in img_names]
-        img_gender = attr_df.Male.values.tolist()
-        img_gender = [int(v > 0) for v in img_gender]
-        self.labels = dict(zip(img_names, img_gender))
-
-        # add image names with the entire path to them
-        path = '/home/guest/MPL-REU-2022/swapped_attributes/eyes/'
-        os.chdir(path)
-        self.image_names = []
-        self.image_names_ref = {}
-        for im in range(182638, 202600):
+        split_min = floor(split[0] * 202599) + 1
+        split_max = floor(split[1] * 202599) + 1
+        for im in range(split_min, split_max):
             augmented_image = []
             # find all images corresponding to the
             for file in glob.glob(path + str(im) + '/*.jpg'):
@@ -133,8 +59,7 @@ class CelebADataset(Dataset):
         img_gender = attr_df.Male.values.tolist()
         img_gender = [int(v > 0) for v in img_gender]
         self.labels = dict(zip(img_names, img_gender))
-        path = '../../img_high_res/'
-        numImgs = 2048
+        path = '/home/guest/MPL-REU-2022/img_high_res/'
         self.image_names = []
         split_min = floor(split[0] * 202599) + 1
         split_max = floor(split[1] * 202599) + 1
@@ -151,28 +76,6 @@ class CelebADataset(Dataset):
         gender = self.labels[int(self.image_names[idx][-10:-4])]
         label = torch.tensor([1-gender, gender])
         return image, label
-
-
-# def show_batch(b):
-#     labels_map = {0: 'female', 1: 'male'}
-#     imgs, lbls = b
-#     fig = plt.figure()
-#     cols, rows = 4, 4
-#     for i in range(16):
-#         img, lbl = imgs[i], lbls[i]
-#         lbl = lbl.numpy()
-#         fig.add_subplot(rows, cols, i+1)
-#         plt.title(labels_map[int(lbl)], fontsize=10, y=0.92)
-#         plt.axis("off")
-#         plt.imshow(img.numpy().transpose((1, 2, 0)))
-#     plt.show()
-
-
-# celeba_dataset = TrainDataset((0, 0.001))
-# dataloader = DataLoader(celeba_dataset, batch_size=16, num_workers=8, drop_last=True, shuffle=True)
-#
-# for batch in dataloader:
-#     show_batch(batch)
 
 
 # This new version of the data loader takes split as a parameter. split is a 2-tuple
